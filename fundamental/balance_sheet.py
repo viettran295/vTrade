6aadffe935d4b3c3d736538d8fa68cacfac93b99
viettr <viettran295@gmail.com>
2026-04-11 12:35:53 +0200

    Update current ratio graph with history data

--- fundamental/balance_sheet.py
+++ fundamental/balance_sheet.py
@@ -1,7 +1,5 @@
 from pydantic import BaseModel
 from typing import Optional
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 
 class FinancialFacts(BaseModel):
@@ -22,20 +20,3 @@ class BalanceSheet(BaseModel):
     total_equity: int = 0
     total_liabilities: int = 0
     financial_facts: Optional[FinancialFacts] = None
-
-    def show_current_ratio(self) -> go.Figure | None:
-        if self.current_assets <= 0 or self.current_liabilities <= 0:
-            return None
-
-        fig = make_subplots(rows=1, cols=2, subplot_titles=("Assets - Liabilities", ""))
-        fig.update_layout(template="plotly_dark", xaxis_rangeslider_visible=False)
-        fig.add_trace(
-            go.Bar(
-                x=["Assets", "Liabilities"],
-                y=[self.current_assets, self.current_liabilities],
-                marker_color=["#198754", "#ff7700"],
-            ),
-            row=1,
-            col=1,
-        )
-        return fig
