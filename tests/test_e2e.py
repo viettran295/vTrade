433dee865f537819283ac990bfa47c5b99805f74
viettr <viettran295@gmail.com>
2026-04-17 21:49:31 +0200

    Increase timeout

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -53,19 +53,19 @@ def test_crossing_ma_graph(page, app_url):
     page.get_by_role("textbox", name="Stock symbol").fill("COIN")
     page.get_by_role("button", name="Search").click()
     # Verify the crossing MA graph exists
-    expect(page.get_by_text("Crossing MA")).to_be_visible(timeout=20000)
-    expect(page.get_by_role("checkbox", name="Crossing MA")).to_be_visible(timeout=20000)
-    expect(page.get_by_text("Short moving average")).to_be_visible(timeout=20000)
-    expect(page.locator("#short-ma-input")).to_be_visible(timeout=20000)
-    expect(page.get_by_text("Long moving average")).to_be_visible(timeout=20000)
-    expect(page.locator("#long-ma-input")).to_be_visible(timeout=20000)
-    expect(page.get_by_text("SMA", exact=True)).to_be_visible(timeout=20000)
-    expect(page.get_by_role("radio", name="SMA")).to_be_visible(timeout=20000)
-    expect(page.get_by_text("EWMA")).to_be_visible(timeout=20000)
-    expect(page.get_by_role("radio", name="EWMA")).to_be_visible(timeout=20000)
-    expect(page.get_by_role("button", name="Apply")).to_be_visible(timeout=20000)
-    expect(page.locator("rect").nth(4)).to_be_visible(timeout=20000)
-    expect(page.locator(".bg")).to_be_visible(timeout=20000)
+    expect(page.get_by_text("Crossing MA")).to_be_visible(timeout=60000)
+    expect(page.get_by_role("checkbox", name="Crossing MA")).to_be_visible(timeout=60000)
+    expect(page.get_by_text("Short moving average")).to_be_visible(timeout=60000)
+    expect(page.locator("#short-ma-input")).to_be_visible(timeout=60000)
+    expect(page.get_by_text("Long moving average")).to_be_visible(timeout=60000)
+    expect(page.locator("#long-ma-input")).to_be_visible(timeout=60000)
+    expect(page.get_by_text("SMA", exact=True)).to_be_visible(timeout=60000)
+    expect(page.get_by_role("radio", name="SMA")).to_be_visible(timeout=60000)
+    expect(page.get_by_text("EWMA")).to_be_visible(timeout=60000)
+    expect(page.get_by_role("radio", name="EWMA")).to_be_visible(timeout=60000)
+    expect(page.get_by_role("button", name="Apply")).to_be_visible(timeout=60000)
+    expect(page.locator("rect").nth(4)).to_be_visible(timeout=60000)
+    expect(page.locator(".bg")).to_be_visible(timeout=60000)
 
 def test_bollingerbands_graph(page, app_url):
     """
@@ -77,11 +77,11 @@ def test_bollingerbands_graph(page, app_url):
     page.get_by_role("textbox", name="Stock symbol").click()
     page.get_by_role("textbox", name="Stock symbol").fill("NVDA")
     page.get_by_role("button", name="Search").click()
-    expect(page.get_by_text("Bollinger bands")).to_be_visible(timeout=20000)
-    expect(page.get_by_role("checkbox", name="Bollinger bands")).to_be_visible(timeout=20000)
+    expect(page.get_by_text("Bollinger bands")).to_be_visible(timeout=60000)
+    expect(page.get_by_role("checkbox", name="Bollinger bands")).to_be_visible(timeout=60000)
     page.get_by_role("checkbox", name="Bollinger bands").check()
     # Verify the Bollinger bands graph exists
-    expect(page.locator("#bb-graph > .js-plotly-plot > .plot-container > .user-select-none > svg > .draglayer > .xy > .nsewdrag")).to_be_visible(timeout=20000)
+    expect(page.locator("#bb-graph > .js-plotly-plot > .plot-container > .user-select-none > svg > .draglayer > .xy > .nsewdrag")).to_be_visible(timeout=60000)
 
 def test_rsi_graph(page, app_url):
     """
@@ -93,11 +93,11 @@ def test_rsi_graph(page, app_url):
     page.get_by_role("textbox", name="Stock symbol").click()
     page.get_by_role("textbox", name="Stock symbol").fill("GOOG")
     page.get_by_role("button", name="Search").click()
-    expect(page.get_by_text("RSI")).to_be_visible(timeout=20000)
-    expect(page.get_by_role("checkbox", name="RSI")).to_be_visible(timeout=20000)
+    expect(page.get_by_text("RSI")).to_be_visible(timeout=60000)
+    expect(page.get_by_role("checkbox", name="RSI")).to_be_visible(timeout=60000)
     page.get_by_role("checkbox", name="RSI").check()
     # Verify the Bollinger bands graph exists
-    expect(page.locator("#rsi-graph > .js-plotly-plot > .plot-container > .user-select-none > svg > .draglayer > .xy > .nsewdrag")).to_be_visible(timeout=20000)
+    expect(page.locator("#rsi-graph > .js-plotly-plot > .plot-container > .user-select-none > svg > .draglayer > .xy > .nsewdrag")).to_be_visible(timeout=60000)
 
 def test_fundamental_graph(page, app_url):
     """
@@ -111,4 +111,4 @@ def test_fundamental_graph(page, app_url):
     page.get_by_role("button", name="Search").click()
     # Click Fundamental tab and verify the graph exists
     page.locator("#fa-id").click()
-    expect(page.locator("rect").nth(4)).to_be_visible(timeout=20000)
+    expect(page.locator("rect").nth(4)).to_be_visible(timeout=60000)
