580dfd62d4c9c98191b6078df8014f9219555127
viettr <viettran295@gmail.com>
2026-04-17 21:26:29 +0200

    Ensure backend service images exist

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,33 +1,52 @@
 import pytest
 import time
+import subprocess
+import docker
 from playwright.sync_api import expect
 from testcontainers.core.container import DockerContainer
 
+def ensure_docker_img_exists(img: str):
+    """
+    Pull docker image if not exists
+    """
+    client = docker.from_env()
+    try:
+        client.images.get(img)
+    except Exception:
+        client.images.pull(img)
+
 @pytest.fixture(scope="session")
-def docker_app_url():
-    vtrade = (
-        DockerContainer("viettrann/vtrade:x86_64")
-        .with_kwargs(network_mode="host")
-    )
+def app_url():
+    fundamental_img = "viettrann/fundamental:latest"
+    strategy_processor_img = "viettrann/str-proc:x86_64"
+    ensure_docker_img_exists(fundamental_img)
+    ensure_docker_img_exists(strategy_processor_img)
     fundamental = (
-        DockerContainer("viettrann/fundamental:latest")
+        DockerContainer(fundamental_img)
         .with_kwargs(network_mode="host")
     )
     strategy_processor = (
-        DockerContainer("viettrann/str-proc:x86_64")
+        DockerContainer(strategy_processor_img)
         .with_kwargs(network_mode="host")
     )
-    with vtrade, fundamental, strategy_processor:
+    with fundamental, strategy_processor:
+        process = subprocess.Popen(
+            ["python", "app.py"],  
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
         host = "localhost"
         port = "8050"
         time.sleep(5)
         yield f"http://{host}:{port}"
+        process.terminate()
+        process.wait()
 
-def test_crossing_ma_graph(page, docker_app_url):
+def test_crossing_ma_graph(page, app_url):
     """
     Test end to end Crossing MA graph
     """
-    page.goto(docker_app_url)
+    page.goto(app_url)
     page.reload()
     # Click to find stock info
     page.get_by_role("textbox", name="Stock symbol").click()
@@ -48,11 +67,11 @@ def test_crossing_ma_graph(page, docker_app_url):
     expect(page.locator("rect").nth(4)).to_be_visible()
     expect(page.locator(".bg")).to_be_visible()
 
-def test_bollingerbands_graph(page, docker_app_url):
+def test_bollingerbands_graph(page, app_url):
     """
     Test end to end Bollinger bands graph
     """
-    page.goto(docker_app_url)
+    page.goto(app_url)
     page.reload()
     # Click to find stock info
     page.get_by_role("textbox", name="Stock symbol").click()
@@ -64,11 +83,11 @@ def test_bollingerbands_graph(page, docker_app_url):
     # Verify the Bollinger bands graph exists
     expect(page.locator("#bb-graph > .js-plotly-plot > .plot-container > .user-select-none > svg > .draglayer > .xy > .nsewdrag")).to_be_visible()
 
-def test_rsi_graph(page, docker_app_url):
+def test_rsi_graph(page, app_url):
     """
     Test end to end fundamental graph
     """
-    page.goto(docker_app_url)
+    page.goto(app_url)
     page.reload()
     # Click to find stock info
     page.get_by_role("textbox", name="Stock symbol").click()
@@ -80,11 +99,11 @@ def test_rsi_graph(page, docker_app_url):
     # Verify the Bollinger bands graph exists
     expect(page.locator("#rsi-graph > .js-plotly-plot > .plot-container > .user-select-none > svg > .draglayer > .xy > .nsewdrag")).to_be_visible()
 
-def test_fundamental_graph(page, docker_app_url):
+def test_fundamental_graph(page, app_url):
     """
     Test end to end fundamental graph
     """
-    page.goto(docker_app_url)
+    page.goto(app_url)
     page.reload()
     # Click to find stock info
     page.get_by_role("textbox", name="Stock symbol").click()
